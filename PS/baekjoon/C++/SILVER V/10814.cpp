ebc5af26bbe7b30cdd609a8a874f072906696df8
cypark728 <cypark448@gmail.com>
2023-01-12 16:59:32 +0900

    2023-01-12

--- PS/baekjoon/C++/SILVER V/10814.cpp
+++ PS/baekjoon/C++/SILVER V/10814.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+struct Imf
+{
+    int order;
+    int age;
+    string name;
+};
+
+bool compare(const Imf& p1, const Imf& p2){
+    if(p1.age == p2.age){
+        return p1.order < p2.order;
+    }else{
+        return p1.age < p2.age;
+    }
+}
 
 int main (){
-    
+    int N;
+
+    cin >> N;
+
+    Imf imf[N];
+
+    for(int i=0; i<N; i++){
+        imf[i].order = i + 1;
+        cin >> imf[i].age;
+        cin >> imf[i].name;
+    }
+
+    sort(imf, imf+N, compare);
+
+    for(int j=0; j<N; j++){
+        cout << imf[j].age << " " << imf[j].name << "\n";
+    }
+
 
 
     return 0;
