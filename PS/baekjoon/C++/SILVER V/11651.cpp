1b72bcb46b646cee0ed275860ccaee83b5e87c26
cypark728 <cypark448@gmail.com>
2023-01-13 14:49:47 +0900

    2023-01-13

---
+++ PS/baekjoon/C++/SILVER V/11651.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct TEMP{
+    int x;
+    int y;
+};
+
+bool compare(TEMP &p1, TEMP&p2){
+    if(p1.y == p2.y){
+        return p1.x < p2.x;
+    }else{
+        return p1.y < p2.y;
+    }
+}
+
+int main (){
+    int N;
+    cin >> N;
+    TEMP temp[N];
+
+    for(int i=0; i<N; i++){
+        cin >> temp[i].x;
+        cin >> temp[i].y;
+    }
+
+    sort(temp, temp+N, compare);
+
+    for(int j=0; j<N; j++){
+        printf("%d %d\n", temp[j].x, temp[j].y);
+    }
+
+    return 0;
+}
