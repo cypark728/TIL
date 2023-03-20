9fe84f8f719162b3f0842ce245b8354e90d10ca6
박찬영 <cypark448@gmail.com>
2023-03-20 07:11:44 +0000

    2023-03-20

---
+++ Language/Python/Unit 9/string.py
@@ -0,0 +1,28 @@
+#문자열 사용하기
+
+#기본사용법
+hello = 'hello world'
+print(hello)
+
+hello = "hello world"
+print(hello)
+
+hello = '''hello world'''
+print(hello)
+
+hello = """hello world"""
+print(hello)
+
+#여러 줄로 된 문자열 사용하기
+hello = '''Hello world
+하이네!
+이세계아이돌화이팅!'''
+print(hello)
+
+#문자열에서 따옴표 사용하기
+hello = "Hello \'python\'"
+print(hello)
+
+#큰 따옴표로 묶으면 작으따옴표가 사용가능하다. 반대도 가능하다.
+hello = 'hello "INE"'
+print(hello)
